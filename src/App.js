8e4c8cf223c81c9a585cace2bdedc4f7456689e4
yabeye <yeabsera.work@gmail.com>
2021-11-18 16:25:02 +0300

    built leftbar structure.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { Grid } from '@material-ui/core';
+import { Grid, makeStyles } from '@material-ui/core';
 import Feed from './components/feed';
 import LeftBar from './components/leftbar';
 import NavBar from './components/navbar';
 import RightBar from './components/rightbar';
 import './App.css';
+
+const useStyles = makeStyles((theme) => ({
+  right: {
+    display: 'block',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <div>
       <NavBar />
@@ -15,7 +26,7 @@ function App() {
         <Grid item sm={7}>
           <Feed />
         </Grid>
-        <Grid item sm={3}>
+        <Grid item sm={3} className={classes.right}>
           <RightBar />
         </Grid>
       </Grid>
--- src/components/leftbar.jsx
+++ src/components/leftbar.jsx
@@ -3,17 +3,36 @@ import { Home } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
   container: {
+    color: 'white',
     paddingTop: theme.spacing(10),
     backgroundColor: theme.palette.primary.main,
     height: '100vh',
+    [theme.breakpoints.up('sm')]: {
+      backgroundColor: 'white',
+      color: '#555',
+      border: '1px solid #ece7e7',
+    },
   },
   items: {
-    color: 'white',
     display: 'flex',
     alignItems: 'center',
     marginBottom: theme.spacing(4),
     [theme.breakpoints.up('sm')]: {
       marginBottom: theme.spacing(3),
+    },
+  },
+  icon: {
+    marginRight: theme.spacing(1),
+    [theme.breakpoints.up('sm')]: {
+      marginRight: 0,
+    },
+  },
+  text: {
+    marginRight: theme.spacing(5),
+    fontWeight: 500,
+    display: 'flex',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
     },
   },
 }));
@@ -23,28 +42,28 @@ function LeftBar() {
   return (
     <Container className={classes.container}>
       <div className={classes.items}>
-        <Home />
-        <Typography>HomePage</Typography>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>HomePage</Typography>
       </div>
       <div className={classes.items}>
-        <Home />
-        <Typography>HomePage</Typography>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>HomePage</Typography>
       </div>
       <div className={classes.items}>
-        <Home />
-        <Typography>HomePage</Typography>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>HomePage</Typography>
       </div>
       <div className={classes.items}>
-        <Home />
-        <Typography>HomePage</Typography>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>HomePage</Typography>
       </div>
       <div className={classes.items}>
-        <Home />
-        <Typography>HomePage</Typography>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>HomePage</Typography>
       </div>
       <div className={classes.items}>
-        <Home />
-        <Typography>HomePage</Typography>
+        <Home className={classes.icon} />
+        <Typography className={classes.text}>HomePage</Typography>
       </div>
     </Container>
   );
