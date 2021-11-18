5ce19dda8f3a69012f0fa768840db34cc663049c
yabeye <yeabsera.work@gmail.com>
2021-11-18 12:01:18 +0300

    material-ui lesson-1: buttons and how to style.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Button, IconButton, makeStyles } from '@material-ui/core';
+import { AddShoppingCart, Person } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    color: '#fff',
+    backgroundColor: theme.palette.success.dark,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div>
       <h1>Heading</h1>
       <p>Some paragraph</p>
-      <Button variant="contained" color="primary">
+
+      <Button variant="contained" color="primary" startIcon={<Person />}>
         SaveMe
+      </Button>
+
+      <br />
+
+      <IconButton color="secondary" aria-label="add to cart">
+        <AddShoppingCart />
+      </IconButton>
+
+      <Button variant="outlined" className={classes.button}>
+        Styled Button
       </Button>
     </div>
   );
